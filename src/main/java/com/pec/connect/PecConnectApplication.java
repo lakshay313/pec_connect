d47655fdd722fe7bac1fc010dafb0361ba4bddce
lakshay313 <lakhshaybansal1998@gmail.com>
2020-04-25 02:28:17 +0530

    reinit repo

---
+++ src/main/java/com/pec/connect/PecConnectApplication.java
@@ -0,0 +1,13 @@
+package com.pec.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PecConnectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/pec/connect/controlllers/HealthController.java
@@ -0,0 +1,16 @@
+package com.pec.connect.controlllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @RequestMapping("/ping")
+    public ResponseEntity<String> ping(){
+        return new ResponseEntity<>("pong", HttpStatus.OK);
+    }
+
+}
